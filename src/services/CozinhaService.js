5494ee8a671a45645013904f141aae3e12279618
Solenir <solenir.figueredo@gmail.com>
2025-11-30 15:26:11 -0300

    feat: adiciona o service da cozinha

---
+++ src/services/CozinhaService.js
@@ -0,0 +1,66 @@
+const { Pedido, Conta, Mesa, ItemPedido, ItemCardapio } = require('../models');
+
+class CozinhaService {
+
+    // Lista todos os pedidos pendentes (status NOVO ou EM_ESPERA)
+    async listarPendentes() {
+        return Pedido.findAll({
+            where: {
+                status: ['PENDENTE']
+            },
+            include: [
+                { model: Conta, attributes: ['nome'], include: [{ model: Mesa, attributes: ['numero'] }] },
+                { 
+                    model: ItemPedido,
+                    as: 'linhasDoPedido',
+                    attributes: ['id', 'quantidade'],
+                    include: [{ model: ItemCardapio, attributes: ['nome', 'preco'] }]
+                }
+            ]
+        });
+    }
+
+    // Marca o pedido como EM_PREPARACAO
+    async iniciarPedido(id) {
+        const pedido = await Pedido.findByPk(id);
+        if (!pedido) throw new Error('Pedido não encontrado');
+
+        if (pedido.status == 'PENDENTE') {
+            pedido.status = 'EM_PREPARACAO';
+           
+        }
+        else{
+            throw new Error('Pedido não pode ser iniciado neste status');
+        }      
+        return await pedido.save();
+    }
+
+    // Marca o pedido como PRONTO
+    async pedidoPronto(id) {
+        const pedido = await Pedido.findByPk(id);
+        if (!pedido) throw new Error('Pedido não encontrado');
+
+        if (pedido.status !== 'EM_PREPARACAO') {
+            throw new Error('Pedido não está em preparação');
+        }
+
+        pedido.status = 'PRONTO';
+        return await pedido.save();
+    }
+
+    // Marca o pedido como ENTREGUE
+    async entregarPedido(id) {
+        const pedido = await Pedido.findByPk(id);
+        if (!pedido) throw new Error('Pedido não encontrado');
+
+        if (pedido.status !== 'PRONTO') {
+            throw new Error('Pedido não está pronto para entrega');
+        }
+
+        pedido.status = 'ENTREGUE';
+        pedido.horarioEntrega = new Date();
+        return await pedido.save();
+    }
+}
+
+module.exports = new CozinhaService();
